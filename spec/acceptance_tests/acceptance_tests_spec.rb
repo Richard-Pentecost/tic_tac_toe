5a7a6636cb590cb17f664f54291a6785f49981f1
Richard Pentecost <richard.pentecost@madetech.com>
2020-09-23 12:00:41 +0100

    game initiates with empty board

--- spec/acceptance_tests/acceptance_tests_spec.rb
+++ spec/acceptance_tests/acceptance_tests_spec.rb
@@ -1,10 +1,11 @@
+require_relative '../../tic_tac_toe.rb'
 
-describe "Game initiation"
+describe "Game initiation" do
     context "player starts game" do
         it "displays an empty board" do
             game = TicTacToe.new
-            grid = "_|_|_\n_|_|_\n_|_|_"
-            expect(TicTacToe.show_board).to output(grid).to_stdout
+            grid = "_|_|_\n_|_|_\n_|_|_\n"
+            expect {game.show_board}.to output(grid).to_stdout
         end
     end
 end
