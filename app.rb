ed342a408a807e3757761c3656fbd3e95a11993e
Claire Guest <claire.guest@madetech.com>
2020-09-29 11:17:57 +0100

    update app file to run game with computer move

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require_relative 'game_controller'
 require_relative 'board'
 require_relative 'tic_tac_toe'
-
+require_relative 'computer'
 
 board = Board.new
 
-game_controller = GameController.new(board)
+computer = Computer.new
+
+game_controller = GameController.new(board, computer)
 
 tic_tac_toe = TicTacToe.new(game_controller)
 
@@ -14,6 +16,8 @@ game_over = tic_tac_toe.game_over
 
 while game_over == false
     tic_tac_toe.show_board
-    tic_tac_toe.get_input
+    tic_tac_toe.player_move
+    tic_tac_toe.computer_move
+
     game_over = tic_tac_toe.game_over
 end
